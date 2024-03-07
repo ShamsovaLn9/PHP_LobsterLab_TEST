cd4fb0939fa20fd27c743726afd3538227ca0f1a
ShamsovaLn9 <157158391+ShamsovaLn9@users.noreply.github.com>
2024-03-07 22:26:27 +0300

    Update mylotterymodule.php

--- mylotterymodule.php
+++ mylotterymodule.php
@@ -21,7 +21,7 @@ class LotteryModule extends Module
         parent::__construct();
 
         $this->displayName = $this->l('Lottery Module');
-        $this->description = $this->l('A module for running a lottery.');
+        $this->description = $this->l('A module for running a daily lottery.');
 
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
     }
@@ -51,6 +51,7 @@ class LotteryModule extends Module
                 id_lottery INT UNSIGNED NOT NULL AUTO_INCREMENT,
                 date DATE NOT NULL,
                 participants INT(10) UNSIGNED NOT NULL DEFAULT 0,
+                winner_declared TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
                 PRIMARY KEY (id_lottery)
             ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;'
         );
@@ -80,18 +81,25 @@ class LotteryModule extends Module
         $row = Db::getInstance()->getRow('SELECT * FROM '._DB_PREFIX_.'lottery WHERE date = \''.$date.'\'');
 
         if (!$row) {
-            Db::getInstance()->execute('INSERT INTO '._DB_PREFIX_.'lottery (date, participants) VALUES (\''.$date.'\', 1)');
+            Db::getInstance()->execute('INSERT INTO '._DB_PREFIX_.'lottery (date, participants, winner_declared) VALUES (\''.$date.'\', 1, 0)');
             $participants = 1;
+            $winnerDeclared = 0;
         } else {
             $participants = $row['participants'] + 1;
+            $winnerDeclared = $row['winner_declared'];
             Db::getInstance()->execute('UPDATE '._DB_PREFIX_.'lottery SET participants = '.$participants.' WHERE date = \''.$date.'\'');
         }
 
-        $isWinner = ($participants === mt_rand(1, $participants));
-        if ($isWinner) {
-            $this->context->smarty->assign('lottery_result', $this->l('Congratulations! You won!'));
+        if (!$winnerDeclared) {
+            $isWinner = ($participants === mt_rand(1, $participants));
+            if ($isWinner) {
+                Db::getInstance()->execute('UPDATE '._DB_PREFIX_.'lottery SET winner_declared = 1 WHERE date = \''.$date.'\'');
+                $this->context->smarty->assign('lottery_result', $this->l('Поздравляю! Ты победил!'));
+            } else {
+                $this->context->smarty->assign('lottery_result', $this->l('Извините, вы не выиграли. Попробуйте еще раз завтра!'));
+            }
         } else {
-            $this->context->smarty->assign('lottery_result', $this->l('Sorry, you did not win. Try again tomorrow!'));
+            $this->context->smarty->assign('lottery_result', $this->l('Победитель на сегодня уже объявлен. Повторите попытку завтра!'));
         }
     }
 }
