dd02a7e541dbad6a76888083f4accf847f4f9763
ShamsovaLn9 <157158391+ShamsovaLn9@users.noreply.github.com>
2024-03-07 17:47:52 +0300

    Update mylotterymodule.php

--- mylotterymodule.php
+++ mylotterymodule.php
@@ -3,11 +3,11 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-class MyLotteryModule extends Module
+class LotteryModule extends Module
 {
     public function __construct()
     {
-        $this->name = 'mylotterymodule';
+        $this->name = 'lotterymodule';
         $this->tab = 'front_office_features';
         $this->version = '1.0.0';
         $this->author = 'Your Name';
@@ -20,20 +20,45 @@ class MyLotteryModule extends Module
 
         parent::__construct();
 
-        $this->displayName = $this->l('My Lottery Module');
-        $this->description = $this->l('A simple lottery module.');
+        $this->displayName = $this->l('Lottery Module');
+        $this->description = $this->l('A module for running a lottery.');
 
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
     }
 
     public function install()
     {
-        if (Shop::isFeatureActive()) {
-            Shop::setContext(Shop::CONTEXT_ALL);
+        if (!parent::install() ||
+            !$this->registerHook('displayTop') ||
+            !$this->installDB()) {
+            return false;
         }
+        return true;
+    }
 
-        return parent::install() &&
-            $this->registerHook('displayTop');
+    public function uninstall()
+    {
+        if (!$this->uninstallDB() || !parent::uninstall()) {
+            return false;
+        }
+        return true;
+    }
+
+    private function installDB()
+    {
+        return Db::getInstance()->execute('
+            CREATE TABLE IF NOT EXISTS '._DB_PREFIX_.'lottery (
+                id_lottery INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                date DATE NOT NULL,
+                participants INT(10) UNSIGNED NOT NULL DEFAULT 0,
+                PRIMARY KEY (id_lottery)
+            ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;'
+        );
+    }
+
+    private function uninstallDB()
+    {
+        return Db::getInstance()->execute('DROP TABLE IF EXISTS '._DB_PREFIX_.'lottery');
     }
 
     public function hookDisplayTop($params)
@@ -43,7 +68,7 @@ class MyLotteryModule extends Module
         }
 
         $this->context->smarty->assign([
-            'lottery_form_url' => $this->context->link->getModuleLink('mylotterymodule', 'display')
+            'lottery_form_url' => $this->context->link->getModuleLink('lotterymodule', 'display')
         ]);
 
         return $this->display(FILE, 'views/templates/hook/lottery_form.tpl');
@@ -51,20 +76,22 @@ class MyLotteryModule extends Module
 
     protected function processLottery()
     {
-        $ipAddress = Tools::getRemoteAddr();
-        $isWinner = $this->isWinner($ipAddress);
+        $date = date('Y-m-d');
+        $row = Db::getInstance()->getRow('SELECT * FROM '._DB_PREFIX_.'lottery WHERE date = \''.$date.'\'');
 
+        if (!$row) {
+            Db::getInstance()->execute('INSERT INTO '._DB_PREFIX_.'lottery (date, participants) VALUES (\''.$date.'\', 1)');
+            $participants = 1;
+        } else {
+            $participants = $row['participants'] + 1;
+            Db::getInstance()->execute('UPDATE '._DB_PREFIX_.'lottery SET participants = '.$participants.' WHERE date = \''.$date.'\'');
+        }
+
+        $isWinner = ($participants === mt_rand(1, $participants));
         if ($isWinner) {
             $this->context->smarty->assign('lottery_result', $this->l('Congratulations! You won!'));
         } else {
             $this->context->smarty->assign('lottery_result', $this->l('Sorry, you did not win. Try again tomorrow!'));
         }
-    }
-
-    protected function isWinner($ipAddress)
-    {
-        $ipLong = ip2long($ipAddress);
-        $hash = crc32($ipLong);
-        return $hash % 10000 === 0; 
     }
 }
